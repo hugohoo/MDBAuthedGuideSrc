b8d73462ffc57e50c899b258a8b01e5570413d84
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-11-18 18:20:01 +0800

    【ch03】 多个匹配记录易导致更新失败

--- ch03/03-update/01--replacedoc.js
+++ ch03/03-update/01--replacedoc.js
@@ -36,3 +36,24 @@ delete joe.enemies;
 db.users.update({"name":"joe"},joe);
 
 db.users.findOne();
+
+/***
+ * 多个匹配记录易导致更新失败
+ ***/
+//创建人
+db.people.insert([
+    {"_id":0,"name":"joe",age:65},
+    {"_id":1,"name":"joe",age:20},
+    {"_id":2,"name":"joe",age:49}
+]);
+
+db.people.find();
+
+//更新
+joe=db.people.findOne({"name":"joe","age":20});
+joe.age++;
+db.people.update({"name":"joe"},joe);
+
+//使用 _id 更新
+db.people.update({"_id":2},joe);
+db.people.find();
