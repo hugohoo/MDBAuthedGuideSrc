9f7d57aaef06899c1bca0cbd5da33b7eb579bac3
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-11-19 14:57:51 +0800

    【ch03】 大批量修改器的效率

---
+++ ch03/04-update-efficiency/02-bulk-changer.js
@@ -0,0 +1,42 @@
+/***
+ *大批量修改器的效率
+ ***/
+
+/***
+ * 批量增长效率
+ ***/
+db.tester.drop();
+db.tester.insert({"x":1});
+db.tester.find();
+var timeInc=function() {
+    var start=(new Date()).getTime();
+
+    for(var i=0;i<10000;i++){
+        db.tester.update({},{
+            "$inc":{"x":1}
+        })
+    }
+
+    var timeDiff=(new Date()).getTime() -start;
+    print("耗时："+timeDiff+"ms");
+}
+
+timeInc();  // 4741ms
+
+/***
+ * 批量插入元素的效率
+ ***/
+var timePush=function() {
+    var start=(new Date()).getTime();
+
+    for(var i=0;i<10000;i++){
+        db.tester.update({},{
+            "$push":{"x":1}
+        })
+    }
+
+    var timeDiff=(new Date()).getTime() -start;
+    print("耗时："+timeDiff+"ms");
+}
+
+timePush(); //9680ms
