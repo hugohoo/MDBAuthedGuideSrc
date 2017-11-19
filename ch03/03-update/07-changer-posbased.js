fee60afb1c7d86acaea80fe1250a80b8d4de0cad
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-11-19 11:51:51 +0800

    【ch03】 通过下标修改指定位置的元素

---
+++ ch03/03-update/07-changer-posbased.js
@@ -0,0 +1,37 @@
+/***
+ * 修改数组指定位置范围内的元素
+ ***/
+
+/***
+ * 通过下标修改指定位置的元素
+ ***/
+db.posts.drop();
+db.posts.insert({
+    "content":"...",
+    "comments":[
+        {
+            "comment":"good post",
+            "author":"John",
+            "votes":0
+        },
+        {
+            "comment":"i thought it was too short",
+            "author":"Claire",
+            "votes":3
+        },
+        {
+            "comment":"free watches",
+            "author":"Alice",
+            "votes":0
+        }
+    ]
+});
+
+db.posts.find();
+
+// 修改第一个平路的投票数量
+db.posts.update({},{
+    "$inc":{
+        "comments.0.votes":1
+    }
+});
