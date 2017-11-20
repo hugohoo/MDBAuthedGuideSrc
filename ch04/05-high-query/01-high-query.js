df17ffa509176e3be45dadd8bfbefd8e0aaf6eb8
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-11-20 11:54:00 +0800

    【ch04】 高级查询

---
+++ ch04/05-high-query/01-high-query.js
@@ -0,0 +1,14 @@
+/***
+ * 高级查询
+ ***/
+// 设置扫描文档上限
+db.coll.find()._addSpecial("$maxscan",5);
+
+// 设置起始文档(必须建立索引)
+db.coll.find()._addSpecial("$min",{"_id":ObjectId("5a1223792fa9655637e0de52"), "x" : 10 });
+
+// 设置结束文档(必须建立索引)
+db.coll.find()._addSpecial("$max",{"_id":ObjectId("5a1223792fa9655637e0de52"), "x" : 10 });
+
+// 查询文档的磁盘位置
+db.coll.find()._addSpecial("$showDiskLoc",true);
