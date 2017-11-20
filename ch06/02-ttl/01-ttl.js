bea68409424ab61a006722dbb4e4ed66611bce00
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-11-20 21:21:06 +0800

    【ch06】 限时索引 TTL

---
+++ ch06/02-ttl/01-ttl.js
@@ -0,0 +1,14 @@
+/***
+ * 限时索引 TTL
+ ***/
+db.foo.drop();
+db.foo.insert({lastUpdated:new Date()});
+db.foo.find();
+
+// 创建时限为一个小时后的限时索引
+db.foo.ensureIndex({lastUpdated:1},{expireAfterSecs:60*60*1});
+
+db.foo.getIndexes();
+
+// 修改限时索引的时限
+db.runCommand({"collMod":"kk.foo",expireAfterSecs:60*60*24});
