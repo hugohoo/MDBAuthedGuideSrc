f5ece4afd2bd77f2acaa1892a73348e105555e0f
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-11-20 18:53:27 +0800

    【ch05】 $or 会尽可能使用每个条件的索引，并将结果集合并去重

--- ch05/03-operator-efficience/01-efficiency.js
+++ ch05/03-operator-efficience/01-efficiency.js
@@ -10,3 +10,8 @@ db.users.find({age:47,username:{
     "$gt":"user5",
     "$lt":"user8"
 }}).explain();
+
+/***
+ * $or 会尽可能使用每个条件的索引，并将结果集合并去重
+ ***/
+db.users.find({"$or":[{age:10},{username:"user500"}]}).explain();
