be5587688e37635b413107e5ca3ef88cf015fea1
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-11-20 08:13:14 +0800

    【ch04】 匹配子文档的多个字段

--- ch04/03-special-query/04-inner-doc.js
+++ ch04/03-special-query/04-inner-doc.js
@@ -20,3 +20,35 @@ db.people.find({name:{first:"Joe",last:"Schemoe"}});
 
 // 点语法匹配子文档
 db.people.find({"name.first":"Joe","name.last":"Schemoe"});
+
+/***
+ * 匹配子文档的多个字段
+ ***/
+db.blog.drop();
+db.blog.insert({
+    content:"...",
+    comments:[
+        {
+            author:"joe",
+            score:3,
+            comment:"nice post"
+        },
+        {
+            author:"mary",
+            score:6,
+            comment:"terrible post"
+        },
+    ]
+});
+db.blog.find();
+
+db.blog.find(
+    {comments:
+        {
+            "$elemMatch":
+                {
+                    author:"joe",
+                    score:{"$gt":2}
+                }
+        }
+    });
