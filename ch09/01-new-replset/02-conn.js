aa60db4324f078590b4a2d4dd331b8954bad52e9
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-11-22 10:59:59 +0800

    【ch09】 连接到 从服务

--- ch09/01-new-replset/02-conn.js
+++ ch09/01-new-replset/02-conn.js
@@ -21,3 +21,22 @@ for (var i=0;i<100;i++) {
     masterDB.coll.insert({count:i});
 }
 masterDB.coll.count();
+
+/***
+ * 连接到 从服务
+ ***/
+mongo --nodb
+secondaryConn=new Mongo("localhost:20001");
+secondaryDB=secondaryConn.getDB("kk");
+
+// 尝试查询 主服务 更新的数据
+secondaryDB.coll.findOne(); //被拒绝
+
+// 开启 从服务 查询
+secondaryConn.setSlaveOk();
+
+// 再次从 从服务 查询
+secondaryDB.coll.findOne();//可以啦
+
+// 尝试从 从服务 上写
+secondaryDB.coll.insert({count:101});//失败，被拒绝
