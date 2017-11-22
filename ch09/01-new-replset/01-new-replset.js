c7476c372e3cd3780e6f0b0d6acf1df33de62189
DESKTOP-6POJJMP\kiky <huafeihua123xxx@gmail.com>
2017-11-22 10:20:08 +0800

    【ch09】 创建副本集

---
+++ ch09/01-new-replset/01-new-replset.js
@@ -0,0 +1,15 @@
+/***
+ * 创建副本集
+ ***/
+// 无连接启动客户端
+mongo --nodb
+
+//创建一个副本集：1主，2从
+replicaSet=new ReplSetTest({"nodes":3});
+
+
+// 启动3个服务进程
+replicaSet.statrtSet();
+
+// 初始化 副本集 的配资
+replicaSet.initiate();
